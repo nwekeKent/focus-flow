493896862e6686ce06af9a2e7c84218f18e561c9
nwekekent <nwekep14@gmail.com>
2026-01-19 13:35:46 +0100

    update some packages

--- components/timer.tsx
+++ components/timer.tsx
@@ -9,7 +9,7 @@ export default function Timer() {
   const { activeColor, activeFont } = useAppContext();
   const { progress, isActive, toggleTimer, formatTime } = useTimer();
 
-  const size = 260;
+  const size = 240;
   const strokeWidth = 10;
   const center = size / 2;
   const radius = (size - strokeWidth) / 2;
@@ -17,40 +17,42 @@ export default function Timer() {
   const strokeDashoffset = circumference * (1 - progress);
 
   return (
-    <View className="w-full mt-20 mb-12 max-w-[300px] h-[300px] md:w-[410px] md:h-[410px] md:max-w-[410px] rounded-[410px] items-center justify-center relative">
-      <View className="w-full h-full bg-secondary-navy-200 rounded-[100%] flex flex-col justify-center items-center relative">
-        <Svg
-          width={size}
-          height={size}
-          style={{ transform: [{ rotate: "90deg" }, { scaleX: -1 }] }}
-        >
-          <Circle
-            cx={center}
-            cy={center}
-            r={radius}
-            stroke={colorMapHex[activeColor || "red"]}
-            strokeWidth={strokeWidth}
-            strokeDasharray={circumference}
-            strokeDashoffset={strokeDashoffset}
-            strokeLinecap="round"
-            fill="transparent"
-          />
-        </Svg>
-
-        <View className="absolute  items-center justify-center">
-          <Text
-            className={`text-white tracking-[-3px] text-[70px]  ${boldFontMap[activeFont || "kumbh"]}`}
+    <View className="w-full bg-[#2E325A] mt-20 mb-12 max-w-[300px] h-[300px] md:w-[410px] md:h-[410px] md:max-w-[410px] rounded-full p-5 items-center justify-center relative">
+      <View className="w-full h-full z-10  items-center justify-center relative">
+        <View className="w-full h-full bg-secondary-navy-200 rounded-[100%] flex flex-col justify-center items-center relative">
+          <Svg
+            width={size}
+            height={size}
+            style={{ transform: [{ rotate: "90deg" }, { scaleX: -1 }] }}
           >
-            {formatTime()}
-          </Text>
+            <Circle
+              cx={center}
+              cy={center}
+              r={radius}
+              stroke={colorMapHex[activeColor || "red"]}
+              strokeWidth={strokeWidth}
+              strokeDasharray={circumference}
+              strokeDashoffset={strokeDashoffset}
+              strokeLinecap="round"
+              fill="transparent"
+            />
+          </Svg>
 
-          <Pressable onPress={toggleTimer} className="active:opacity-70">
+          <View className="absolute  items-center justify-center">
             <Text
-              className={`text-white text-xl tracking-[12px]  opacity-80 ${boldFontMap[activeFont || "kumbh"]}`}
+              className={`text-white tracking-[-3px] text-[70px]  ${boldFontMap[activeFont || "kumbh"]}`}
             >
-              {isActive ? "PAUSE" : "START"}
+              {formatTime()}
             </Text>
-          </Pressable>
+
+            <Pressable onPress={toggleTimer} className="active:opacity-70">
+              <Text
+                className={`text-white text-xl tracking-[12px]  opacity-80 ${boldFontMap[activeFont || "kumbh"]}`}
+              >
+                {isActive ? "PAUSE" : "START"}
+              </Text>
+            </Pressable>
+          </View>
         </View>
       </View>
     </View>
